79c6ded4fa7089c49c07a665d4d553053bc3d05d
aboveyunhai <35160613+aboveyunhai@users.noreply.github.com>
2021-09-24 17:09:18 -0400

    Fix: basic test case

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { SingleDatepicker } from '../src';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(<SingleDatepicker date={new Date()} onDateChange={()=>{}}/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
