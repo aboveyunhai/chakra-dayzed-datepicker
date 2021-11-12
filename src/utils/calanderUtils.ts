f993dc984dbf085f1a906e9fd7b3839a7653b72a
aboveyunhai <35160613+aboveyunhai@users.noreply.github.com>
2021-11-12 17:09:38 -0500

    Create calanderUtils.ts

---
+++ src/utils/calanderUtils.ts
@@ -0,0 +1,39 @@
+export const Month_Names_Full = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+export const Month_Names_Short = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+export const Weekday_Names_Short = [
+  'Sun',
+  'Mon',
+  'Tue',
+  'Wed',
+  'Thu',
+  'Fri',
+  'Sat',
+];
