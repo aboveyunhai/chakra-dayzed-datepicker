7d28725490ad6a9eda7d0a670f253b8a2188b2c7
aboveyunhai <35160613+aboveyunhai@users.noreply.github.com>
2022-10-03 13:05:26 -0400

    update example

--- example/index.tsx
+++ example/index.tsx
@@ -1,50 +1,51 @@
 import 'react-app-polyfill/ie11';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ChakraProvider, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  ChakraProvider,
+  Heading,
+  HStack,
+  VStack,
+} from '@chakra-ui/react';
 import { SingleDatepicker, RangeDatepicker } from '../src';
 import { useState } from 'react';
-import { Month_Names_Full } from '../src/utils/calanderUtils';
+import { DatepickerConfigs } from '../src/utils/commonTypes';
+import { Weekday_Names_Short } from '../src/utils/calanderUtils';
+
+type FirstDayOfWeek = DatepickerConfigs['firstDayOfWeek'];
+const offsets: FirstDayOfWeek[] = [0, 1, 2, 3, 4, 5, 6];
 
 const App = () => {
-  const [date, setDate] = useState(new Date("07/28/2022"));
+  const [date, setDate] = useState(new Date('07/28/2022'));
   const [selectedDates, setSelectedDates] = useState<Date[]>([
     new Date(),
     new Date(),
   ]);
+  const [firstDayOfWeek, setFirstDayOfWeek] = useState<FirstDayOfWeek>(1);
   return (
     <ChakraProvider>
-      <VStack padding={'1em'} spacing={5} minHeight={'600px'}>
+      <VStack
+        padding={'1em'}
+        spacing={5}
+        minHeight={'600px'}
+        alignItems="flex-start"
+      >
+        <Heading>Single:</Heading>
         <SingleDatepicker
           name="date-input"
           date={date}
-          minDate={new Date("07/25/2022")}
-          maxDate={new Date("08/05/2022")}
+          minDate={new Date('07/25/2022')}
+          maxDate={new Date('08/05/2022')}
           onDateChange={setDate}
         />
-        <SingleDatepicker
-          name="date-input"
-          date={date}
-          onDateChange={setDate}
-          configs={{
-            dateFormat: 'yyyy-MM-dd',
-            dayNames: [
-              'Mon',
-              'Tue',
-              'Wed',
-              'Thu',
-              'Fri',
-              'Sat',
-              'Sun',
-            ],
-            monthNames: Month_Names_Full,
-            firstDayOfWeek: 1,
-          }}
-        />
+        <Heading>Range:</Heading>
         <RangeDatepicker
           selectedDates={selectedDates}
           onDateChange={setSelectedDates}
         />
+        <Heading>Custom Styles:</Heading>
         <RangeDatepicker
           selectedDates={selectedDates}
           onDateChange={setSelectedDates}
@@ -61,14 +62,17 @@ const App = () => {
                 },
               },
               isInRangeBtnProps: {
-                color: 'yellow',
+                color: 'purple.800',
+                borderColor: 'blue.300',
               },
               selectedBtnProps: {
                 background: 'blue.200',
-                color: 'green',
+                borderColor: 'blue.300',
+                color: 'blue.600',
               },
               todayBtnProps: {
-                background: 'teal.400',
+                background: 'teal.200',
+                color: 'teal.700',
               },
             },
             inputProps: {
@@ -76,33 +80,42 @@ const App = () => {
             },
           }}
         />
+        <Heading>Custom Month/Weekday/Format:</Heading>
         <RangeDatepicker
           selectedDates={selectedDates}
           onDateChange={setSelectedDates}
-          defaultIsOpen={true}
           configs={{
             dateFormat: 'yyyy-MM-dd',
-            dayNames: Array(7).fill('day'),
-            monthNames: Array(12).fill('month'),
+            dayNames: 'abcdefg'.split(''), // length of 7
+            monthNames: 'ABCDEFGHIJKL'.split(''), // length of 12
             firstDayOfWeek: 0,
+          }}
+        />
+        <Heading>With Offset:</Heading>
+        <Box>First Day of Week: {Weekday_Names_Short[firstDayOfWeek || 0]}</Box>
+        <HStack spacing={1}>
+          {offsets.map((e) => (
+            <Button
+              key={e}
+              onClick={() => setFirstDayOfWeek(e)}
+              backgroundColor={firstDayOfWeek === e ? 'green.300' : undefined}
+            >
+              {e}
+            </Button>
+          ))}
+        </HStack>
+        <SingleDatepicker
+          name="date-input"
+          date={date}
+          onDateChange={setDate}
+          configs={{
+            firstDayOfWeek,
           }}
         />
         <RangeDatepicker
           selectedDates={selectedDates}
           onDateChange={setSelectedDates}
-          defaultIsOpen={true}
           configs={{
-            dateFormat: 'yyyy-MM-dd',
-            dayNames: [
-              'Mon',
-              'Tue',
-              'Wed',
-              'Thu',
-              'Fri',
-              'Sat',
-              'Sun',
-            ],
-            monthNames: Array(12).fill('month'),
             firstDayOfWeek: 1,
           }}
         />
