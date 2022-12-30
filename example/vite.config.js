0dcec081e880aff19008180f87c39dcbffb89f00
aboveyunhai <35160613+aboveyunhai@users.noreply.github.com>
2022-12-30 18:37:51 -0500

    add base url for gh-pages

--- example/vite.config.js
+++ example/vite.config.js
@@ -3,9 +3,15 @@ import ReactPlugin from 'vite-preset-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/chakra-dayzed-datepicker/",
   plugins: [
     ReactPlugin({
       injectReact: false,
     }),
   ],
+  server: {
+    fs: {
+      allow: ['..']
+    }
+  }
 });
