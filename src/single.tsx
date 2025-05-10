86817c6f61df798ee70c9dfffe81e54a620c23e0
Barbér Máté <mate1024@gmail.com>
2025-05-11 00:57:45 +0200

    Fix error

--- src/single.tsx
+++ src/single.tsx
@@ -65,7 +65,7 @@ export type VariantProps =
 
 export type SingleDatepickerProps = SingleProps & VariantProps;
 
-const DefaultConfigs: Required<DatepickerConfigs> = {
+const DefaultConfigs: Required<Omit<DatepickerConfigs, 'emptyDatePlaceholder'>> = {
   dateFormat: 'yyyy-MM-dd',
   monthNames: Month_Names_Short,
   dayNames: Weekday_Names_Short,
